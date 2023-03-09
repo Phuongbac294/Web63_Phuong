58ffca896a14118c09d585d4ebb5ac36024ce0b8
Phuongbac294 <syphuong83@gmail.com>
2023-03-09 16:23:13 +0700

    up len git

---
+++ habits/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Menu from './menu/menu';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Xin chào bạn đến với Habits Shop</h1>
+      <Menu/>
+    </div>
+  );
+}
+
+export default App;
---
+++ habits/src/menu/menu.js
@@ -0,0 +1,19 @@
+function Menu() {
+    return (
+        <div>
+            <ul>
+                <li>Habits <span color="red">Shop</span></li>
+                <li>
+                    <input type="text" placeholder="Tìm kiếm"></input>
+                </li>
+                <li>Login</li>
+                <li>
+                    Giỏ hàng
+                    
+                </li>
+            </ul>
+        </div>
+    )
+}
+
+export default Menu;
